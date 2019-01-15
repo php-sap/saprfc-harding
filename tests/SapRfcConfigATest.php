81b42871d510de94bd4c1cee17f7831e3e2d8b62
Gregor <gregor-j@users.noreply.github.com>
2019-01-15 15:07:00 +0100

    change integration tests to dev-php5 and add module specific config assertion

--- tests/SapRfcConfigATest.php
+++ tests/SapRfcConfigATest.php
@@ -34,4 +34,50 @@ class SapRfcConfigATest extends AbstractConfigATestCase
     {
         return new SapRfcConfigA($config);
     }
+
+    /**
+     * Assert the actual module configuration variable.
+     * @param array $configSaprfc
+     * @param string $ashost
+     * @param string $sysnr
+     * @param string $client
+     * @param string $user
+     * @param string $passwd
+     * @param string $gwhost
+     * @param string $gwserv
+     * @param string $lang
+     * @param string $trace
+     */
+    public function assertValidModuleConfig(
+        $configSaprfc,
+        $ashost,
+        $sysnr,
+        $client,
+        $user,
+        $passwd,
+        $gwhost,
+        $gwserv,
+        $lang,
+        $trace
+    ) {
+        static::assertInternalType('array', $configSaprfc);
+        static::assertArrayHasKey('ASHOST', $configSaprfc);
+        static::assertSame($ashost, $configSaprfc['ASHOST']);
+        static::assertArrayHasKey('SYSNR', $configSaprfc);
+        static::assertSame($sysnr, $configSaprfc['SYSNR']);
+        static::assertArrayHasKey('CLIENT', $configSaprfc);
+        static::assertSame($client, $configSaprfc['CLIENT']);
+        static::assertArrayHasKey('USER', $configSaprfc);
+        static::assertSame($user, $configSaprfc['USER']);
+        static::assertArrayHasKey('PASSWD', $configSaprfc);
+        static::assertSame($passwd, $configSaprfc['PASSWD']);
+        static::assertArrayHasKey('GWHOST', $configSaprfc);
+        static::assertSame($gwhost, $configSaprfc['GWHOST']);
+        static::assertArrayHasKey('GWSERV', $configSaprfc);
+        static::assertSame($gwserv, $configSaprfc['GWSERV']);
+        static::assertArrayHasKey('LANG', $configSaprfc);
+        static::assertSame($lang, $configSaprfc['LANG']);
+        static::assertArrayHasKey('TRACE', $configSaprfc);
+        static::assertSame($trace, $configSaprfc['TRACE']);
+    }
 }
